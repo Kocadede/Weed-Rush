84d442bbab86d7085220049f35868e0c0a4212dc
Kocadede <63886962+Kocadede@users.noreply.github.com>
2021-01-16 19:56:26 +0300

    Add files via upload

---
+++ items.sql
@@ -0,0 +1,9 @@
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('wet', 'Wet', 3, 0, 1),
+	('weedbod', 'Weedbod', 2, 0, 1),
+	('weedbud', 'Weedbud', 2, 0, 1),
+	('joint', 'Joint', 2, 0, 1)
+;
+
